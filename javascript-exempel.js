8c095f2a9190b9f75234d37f5453097a3a2572d1
Tobias Lindberg <tobias.lindberg@ga.ntig.se>
2021-03-28 23:22:57 +0200

    javaScript-exempel

--- javascript-exempel.js
+++ javascript-exempel.js
@@ -4,4 +4,14 @@ som kan sträcka sig över flera rader. */
 //En kommentar på en rad.
 function visaDialog(){
     alert('Hej på dej!');
+    changeText();
+}
+
+function changeText(){
+    document.getElementById("demo").innerHTML = "Hello javaScript;";
+}
+
+function writeText(){
+    var namn = document.getElementById("fnamn").value;
+    document.getElementById("text").innerHTML = namn;
 }
